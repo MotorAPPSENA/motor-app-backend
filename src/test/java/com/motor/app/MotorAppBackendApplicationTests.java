71796c2471493c5a98c1809ada21858d05c6a3c6
Velasquez076 <motorappficha807@gmail.com>
2024-02-21 22:00:21 -0500

    Add - first commit

---
+++ src/test/java/com/motor/app/MotorAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
