c2605610a77b1a08606ab587e3e8d644d201ad99
MotorAPPSENA <motorappficha807@gmail.com>
2024-02-28 21:21:45 -0500

    se agrega entidad Car

---
+++ src/main/java/com/motor/app/persistence/models/Car.java
@@ -0,0 +1,68 @@
+package com.motor.app.persistence.models;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/*
+ * 
+ */
+
+@Entity
+@Table(name="car")
+public class Car {
+  
+@Column (name="mtr_car_id")  
+private Long carId;
+
+@Column (name="mtr_customer_id_fk")  
+private Long customerId;
+
+@Column (name="mtr_creation_date_car")  
+private LocalDateTime creationDateCar;
+
+public Car(LocalDateTime creationDateCar) {
+  super();
+  this.creationDateCar = creationDateCar;
+}
+
+public Long getCarId() {
+  return carId;
+}
+
+public void setCarId(Long carId) {
+  this.carId = carId;
+}
+
+public Long getCustomerId() {
+  return customerId;
+}
+
+public void setCustomerId(Long customerId) {
+  this.customerId = customerId;
+}
+
+public LocalDateTime getCreationDateCar() {
+  return creationDateCar;
+}
+
+public void setCreationDateCar(LocalDateTime creationDateCar) {
+  this.creationDateCar = creationDateCar;
+}
+
+@Override
+public String toString() {
+  return "Car [carId=" + carId + ", customerId=" + customerId + ", creationDateCar="
+      + creationDateCar + "]";
+}
+
+
+
+
+
+
+
+  
+  
+}
